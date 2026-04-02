66fbfb5edb149df8b0de11a15aa40cf9f8b67d14
Arthur Salles <112512699+arthurjaexiste@users.noreply.github.com>
2026-04-02 17:19:49 -0300

    print

--- GOLANG/AULAS/001/main.go
+++ GOLANG/AULAS/001/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+	
 	fmt.Println("hello world")
-}
-
-func init() {
-	fmt.Println("teste")
-}
-
-// na linguagem go ele sempre vai printar a func init primeiro
+	
+};
