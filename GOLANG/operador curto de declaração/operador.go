eb5fb3c9483fd2850cb5a8d85fbc7d1e262fd580
Arthur Salles <112512699+arthurjaexiste@users.noreply.github.com>
2026-04-30 09:41:15 -0300

    operador

--- GOLANG/operador curto de declaração/operador.go
+++ GOLANG/operador curto de declaração/operador.go
@@ -4,10 +4,18 @@ package main
 
 import "fmt"
 
+
+
 func main(){
 
 	x :=  10   //parece uma marmota kkkk
-	y := "ola Bom Dia"
-	fmt.Println(x)
+	y := "ola Bom Dia"   // := só funciona dentro de escopo 
+	fmt.Println(x,y)
 
+
+	fmt.Println(y)
+
+	x = 20
+
+	fmt.Println(x)
 }
